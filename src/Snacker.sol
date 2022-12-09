9812218c70741ce0dcd7002c8d4ba61f027b0f82
0xNotes <notes@sigil.systems>
2022-12-09 01:19:56 -0500

    Added burn function

--- src/Snacker.sol
+++ src/Snacker.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+
 contract Snacker {
     // uint256 public number;
 
@@ -19,8 +22,14 @@ contract Snacker {
 
 
     
-    function SnackApproval(address _contentNft, uint256 _contentId, address _styleNft, uint256 _styleId) public {
-        _contentNft.approve(address(this), _contentId);
-        _styleNft.approve(address(this), _styleId);
+    // function SnackApproval(address _contentNft, uint256 _contentId, address _styleNft, uint256 _styleId) public {
+    //     IERC721(_contentNft).approve(address(this), _contentId);
+    //     IERC721(_styleNft).approve(address(this), _styleId);
+    // }
+
+    function Snack(address _contentNft, uint256 _contentId, address _styleNft, uint256 _styleId) public {
+        IERC721(_contentNft).transferFrom(msg.sender, burn, _contentId);
+        IERC721(_styleNft).transferFrom(msg.sender, burn, _styleId);
+        
     }
 }
