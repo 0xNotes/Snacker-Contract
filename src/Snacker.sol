81d2661f72684256b10a52aded8919e27384b36e
0xNotes <notes@sigil.systems>
2022-12-10 01:13:30 -0500

    More Robust Snacker

--- src/Snacker.sol
+++ src/Snacker.sol
@@ -1,36 +1,57 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 
-contract Snacker {
-    // uint256 public number;
+contract Snacker is ERC721, ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
 
-    // function setNumber(uint256 newNumber) public {
-    //     number = newNumber;
-    // }
+    Counters.Counter private _tokenIdCounter;
 
-    // function increment() public {
-    //     number++;
-    // }
-
+    constructor() ERC721("Snacker", "SNK") {}
     address public burn = 0x000000000000000000000000000000000000dEaD;
 
 
-    
 
-
-    
-    // function SnackApproval(address _contentNft, uint256 _contentId, address _styleNft, uint256 _styleId) public {
-    //     IERC721(_contentNft).approve(address(this), _contentId);
-    //     IERC721(_styleNft).approve(address(this), _styleId);
-    // }
-
-    function Snack(address _contentNft, uint256 _contentId, address _styleNft, uint256 _styleId) public {
+    function Snack(address _contentNft, uint256 _contentId, address _styleNft, uint256 _styleId, address to, string memory uri) public {
         IERC721(_contentNft).transferFrom(msg.sender, burn, _contentId);
         IERC721(_styleNft).transferFrom(msg.sender, burn, _styleId);
         
-        
+        uint256 tokenId = _tokenIdCounter.current();
+
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    function safeMint(address to, uint256 tokenId, string memory uri)
+        public
+        onlyOwner
+    {
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
     }
 }
+
+
+
+
