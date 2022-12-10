c6ecd8dd5264e77081d38d2cbc4794cc953e4f56
0xNotes <notes@sigil.systems>
2022-12-10 01:54:35 -0500

    More updates

--- src/Snacker.sol
+++ src/Snacker.sol
@@ -16,13 +16,20 @@ contract Snacker is ERC721, ERC721URIStorage, Ownable {
 
     constructor() ERC721("Snacker", "SNK") {}
     address public burn = 0x000000000000000000000000000000000000dEaD;
+    mapping(address => uint) public burnCount;
 
 
 
     function Snack(address _contentNft, uint256 _contentId, address _styleNft, uint256 _styleId, address to) public {
         IERC721(_contentNft).transferFrom(msg.sender, burn, _contentId);
         IERC721(_styleNft).transferFrom(msg.sender, burn, _styleId);
-        
+
+        burnCount[_contentNft] = burnCount[_contentNft] + 1;
+        burnCount[_styleNft] = burnCount[_styleNft] + 1;
+
+        //This is where the NFT needs to be generated from external adapter
+        //The resulting ipfs link should be given back to the _setTokenUri or something
+
         uint256 tokenId = _tokenIdCounter.current();
 
         _tokenIdCounter.increment();
@@ -30,13 +37,7 @@ contract Snacker is ERC721, ERC721URIStorage, Ownable {
         _setTokenURI(tokenId, uri);
     }
 
-    function safeMint(address to, uint256 tokenId, string memory uri)
-        public
-        onlyOwner
-    {
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
-    }
+
 
     // The following functions are overrides required by Solidity.
 
